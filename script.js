84e5d4edbd12e3a928549701f53537e139f701b0
affan-ai <affanianamara@gmail.com>
2024-03-02 13:53:52 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const navLinks = document.querySelectorAll('.navbar a');
+
+  navLinks.forEach(link => {
+      link.addEventListener('click', function(e) {
+          e.preventDefault();
+
+          const targetId = this.getAttribute('href').substring(1);
+          const targetSection = document.getElementById(targetId);
+
+          window.scrollTo({
+              top: targetSection.offsetTop,
+              behavior: 'smooth' // Menambahkan efek animasi smooth
+          });
+      });
+  });
+});
